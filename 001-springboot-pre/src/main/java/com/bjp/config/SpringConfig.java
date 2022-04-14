7ef1f961d02602a2a70126fbd5783aff26a54cdb
xuhan <821602702@qq.com>
2022-04-14 16:01:26 +0800

    用javaConfig来配置容器

---
+++ 001-springboot-pre/src/main/java/com/bjp/config/SpringConfig.java
@@ -0,0 +1,50 @@
+package com.bjp.config;
+
+import com.bjp.vo.Student;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Configuration:表示当前类是作为配置文件使用的。 就是用来配置容器的
+ *       位置：在类的上面
+ *
+ *  SpringConfig这个类就相当于beans.xml
+ *
+ */
+@Configuration
+public class SpringConfig {
+    /**
+     * 创建方法，方法的返回值是对象。 在方法的上面加入@Bean
+     * 方法的返回值对象就注入到容器中。
+     *
+     * @Bean: 把对象注入到spring容器中。 作用相当于<bean>
+     *     也就是吧返回值放入容器之中
+     *
+     *     位置：方法的上面
+     *
+     *     说明：@Bean,不指定对象的名称，默认是方法名是 id
+     *
+     */
+    @Bean
+    public Student createStudent(){
+        Student student = new Student();
+        student.setName("zhangsan");
+        student.setAge(34);
+        student.setSex("男");
+        return student;
+    }
+
+    /***
+     * 指定对象在容器中的名称（指定<bean>的id属性）
+     * @Bean的name属性，指定对象的名称（id）
+     */
+    @Bean(name = "lisiStudent")
+    public Student makeStudent(){
+        Student s2  = new Student();
+        s2.setName("李四");
+        s2.setAge(22);
+        s2.setSex("男");
+        return s2;
+    }
+}
--- 001-springboot-pre/src/test/java/com/bjp/MyTest.java
+++ 001-springboot-pre/src/test/java/com/bjp/MyTest.java
@@ -1,23 +1,71 @@
 package com.bjp;
 
+import com.bjp.config.SpringConfig;
 import com.bjp.vo.Student;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
+
+
 
     /**
      * 使用xml作为容器的配置文件
      */
     @Test
-    public void test(){
-        String config="beans.xml";
+    public void test1() {
+        String config = "beans.xml";
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(config);
         Student myStudent = (Student) ctx.getBean("myStudent");
-        System.out.println("容器中的对象："+myStudent);
+        System.out.println("容器中的对象：" + myStudent);
 
+    }
+
+    @Test
+    public void tes02() {
+        //形参类型：Class<?>... componentClasses 也就是说要有个组建类（java对象）的class类
+        //那SpringConfig这个类里面有java对象 就是那个bean
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        Student student = (Student) ctx.getBean("createStudent");
+        System.out.println("使用JavaConfig创建的bean对象：" + student);
+    }
+
+    @Test
+    public void test03(){
+        ApplicationContext ctx=new AnnotationConfigApplicationContext(SpringConfig.class);
+        Student lisi= (Student) ctx.getBean("lisiStudent");
+        System.out.println("使用JavaConfig创建的bean对象：" + lisi);
 
 
     }
+
+
+
+    /**
+     * 许晗自己添加的测试
+     */
+    @Test
+    public void test01(){
+        String config="beans.xml";
+        ApplicationContext ctx=new ClassPathXmlApplicationContext(config);
+        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("容器中的名称："+beanDefinitionName+"||"+ctx.getBean(beanDefinitionName));
+        }
+    }
+
+    @Test
+    public void test02(){
+
+        ApplicationContext ctx=new AnnotationConfigApplicationContext(SpringConfig.class);
+        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("容器中的名称："+beanDefinitionName+"||"+ctx.getBean(beanDefinitionName));
+        }
+    }
+
+
 
 }
