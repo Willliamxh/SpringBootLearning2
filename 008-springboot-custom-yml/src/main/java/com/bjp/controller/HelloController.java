df2e5e4d75b511d235dac39485063c42f8ff8114
xuhan <821602702@qq.com>
2022-04-18 11:08:33 +0800

    使用@ConfigurationProperties和@Component还有@Resource，将配置文件的数据映射为java类，也就是SchoolInfo这个类，其中@ConfigurationProperties(prefix="")负责注入配置文件的信息

--- 008-springboot-custom-yml/src/main/java/com/bjp/controller/HelloController.java
+++ 008-springboot-custom-yml/src/main/java/com/bjp/controller/HelloController.java
@@ -1,9 +1,12 @@
 package com.bjp.controller;
 
+import com.bjp.vo.SchoolInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 @Controller
 public class HelloController {
@@ -21,10 +24,23 @@ public class HelloController {
     @Value("${site}")
     private String site;
 
+    //自动注入，获取一个SchoolInfo对象类赋值给info
+    //autowired默认是按bytype，resource默认是byname
+    @Resource
+    private SchoolInfo info;
+
 
     @RequestMapping("/data")
     @ResponseBody
     public String queryData(){
         return name+",site="+site+", 项目的访问地址="+contextPath+",使用的端口="+port;
     }
+
+    @RequestMapping("/info")
+    @ResponseBody
+    public String  queryInfo(){
+        return "SchoolInfo对象=="+info.toString();
+    }
+
+
 }
