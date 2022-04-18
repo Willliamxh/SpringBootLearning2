77896fb86538d46ed20e41b36d0405c0b6f0d4c1
xuhan <821602702@qq.com>
2022-04-18 15:31:28 +0800

    创建jsp项目，jsp这块知识不需要太了解，就不多做笔记了。

---
+++ ch009-springboot-jsp/src/main/java/com/bjp/Ch009SpringbootJspApplication.java
@@ -0,0 +1,13 @@
+package com.bjp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch009SpringbootJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch009SpringbootJspApplication.class, args);
+	}
+
+}
