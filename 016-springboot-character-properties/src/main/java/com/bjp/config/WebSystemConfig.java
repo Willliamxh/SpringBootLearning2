d3ab769729aac5d8aa042543ba70513cdfe898d6
xuhan <821602702@qq.com>
2022-04-19 17:05:56 +0800

    解决乱码方法2：修改系统默认的的过滤器配置，设置 #让系统的CharacterEncdoingFilter生效 server.servlet.encoding.enabled=true #指定使用的编码方式 server.servlet.encoding.charset=utf-8 #强制request，response都使用charset属性的值 server.servlet.encoding.force=true

---
+++ 016-springboot-character-properties/src/main/java/com/bjp/config/WebSystemConfig.java
@@ -0,0 +1,22 @@
+package com.bjp.config;
+
+import com.bjp.web.MyServlet;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+public class WebSystemConfig {
+
+    //注册Servlet
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        MyServlet myServlet = new MyServlet();
+        ServletRegistrationBean reg = new ServletRegistrationBean(myServlet,
+                "/myservlet");
+        return reg;
+    }
+
+}
