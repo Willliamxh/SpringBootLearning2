8f185a477019dab90198d5f78e1f6783e64ed4b8
xuhan <821602702@qq.com>
2022-04-22 19:52:04 +0800

    配置文件测试修改，mapper的文件名称一定要和dao文件保持一致，目前还没学到mapper注解怎么用，估计有对应的映射关系

--- 017-springboot-mapper/src/main/java/com/bjp/Application.java
+++ 017-springboot-mapper/src/main/java/com/bjp/Application.java
@@ -1,13 +1,23 @@
 package com.bjp;
 
+import com.bjp.model.Student;
+import com.bjp.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		// SpringApplication.run(Application.class, args);
+		//获取容器对象
+		ConfigurableApplicationContext ctx=SpringApplication.run(Application.class, args);
+		// 从容器中获取对象
+		StudentService userService=(StudentService) ctx.getBean("StudentServiceImpl");
+		Student student = userService.queryStudent(1);
+		System.out.println(student);
+
 	}
 
 }
--- 017-springboot-mapper/src/main/java/com/bjp/service/impl/StudentServiceImpl.java
+++ 017-springboot-mapper/src/main/java/com/bjp/service/impl/StudentServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-@Service
+@Service("StudentServiceImpl")
 public class StudentServiceImpl implements StudentService {
 
     @Resource
