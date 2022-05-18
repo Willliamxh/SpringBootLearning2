e47334af54ac55e5b504ec5c76d1d69a6e13aa57
Williamxh <821602702@qq.com>
2022-05-18 11:05:24 +0800

    6.2 添加数据到redis, 1.使用redisTemplate去获取对应的数据，发现redis中的数据有乱码。其把k，v 经过了序列化存到redis。 k，v 是序列化的内容， 不能直接识别. 2.StringRedisTemplate ： 把k，v 都是作为String处理， 使用的是String的序列化 ， 可读性好

--- 021-springboot-redis/src/main/java/com/bjp/controller/RedisController.java
+++ 021-springboot-redis/src/main/java/com/bjp/controller/RedisController.java
@@ -1,8 +1,12 @@
 package com.bjp.controller;
 
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.annotation.Resources;
@@ -11,6 +15,7 @@ import javax.annotation.Resources;
  * @Author Willam_xh
  * @Date 2022-05-17 19:32
  */
+@RestController
 public class RedisController {
 
     /**
@@ -27,7 +32,13 @@ public class RedisController {
     @Resource
     private RedisTemplate redisTemplate;
 
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     //添加数据到redis
+    //使用postman设定对应的参数和地址
+    //http://localhost:9001/myredis/redis/addString?name=myname&value=lisi
     @PostMapping("/redis/addString")
     public String addToRedis(String name,String value){
         // 操作Redis中string类型的数据， 先获取ValueOperations对象
@@ -41,6 +52,27 @@ public class RedisController {
 
 
     //从redis获取数据
+    @GetMapping("/redis/getk")
+    public String getData(String k){
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        Object v = valueOperations.get(k);
+        return "key是"+k+",他的值为"+v;
+    }
+
+    @PostMapping("/redis/{k}/{v}")
+    public String addStringKV(@PathVariable String k,
+                              @PathVariable String v){
+        //使用StringRedisTemplate对象
+        stringRedisTemplate.opsForValue().set(k,v);
+        return "使用stringRedisTemplate对象";
+    }
+
+    @GetMapping("/redis/getstr/{k}")
+    public String getSringValue(@PathVariable String k){
+        String v = stringRedisTemplate.opsForValue().get(k);
+        return "k的value:"+v;
+    }
+
 
 
 }
