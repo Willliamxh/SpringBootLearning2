fe207d653cf5d46cd1634e200658293926081f42
Williamxh <821602702@qq.com>
2022-05-19 10:44:37 +0800

    7.1创建公共接口项目

---
+++ 022-interface-api/src/main/java/com/bjp/model/Student.java
@@ -0,0 +1,49 @@
+package com.bjp.model;
+
+import java.io.Serializable;
+
+/**
+ * @Author Willam_xh
+ * @Date 2022-05-19 10:32
+ */
+public class Student implements Serializable {
+
+    private static final long serialVersionUID = -9191073376976992000L;
+
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ 022-interface-api/src/main/java/com/bjp/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.bjp.service;
+
+import com.bjp.model.Student;
+
+/**
+ * @Author Willam_xh
+ * @Date 2022-05-19 10:38
+ */
+public interface StudentService {
+
+    Student queryStudent(Integer id);
+
+}
