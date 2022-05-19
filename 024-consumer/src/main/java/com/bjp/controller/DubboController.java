d0fb30b07f17b74d3911e4150331bf532ce159a8
Williamxh <821602702@qq.com>
2022-05-19 19:21:09 +0800

    7.8 消费者调用提供者

---
+++ 024-consumer/src/main/java/com/bjp/controller/DubboController.java
@@ -0,0 +1,30 @@
+package com.bjp.controller;
+
+import com.bjp.model.Student;
+import com.bjp.service.StudentService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Willam_xh
+ * @Date 2022-05-19 17:01
+ */
+
+@RestController
+public class DubboController {
+
+    /**
+     * 引用远程服务， 把创建好的代理对象，注入给studentService
+     */
+    @DubboReference(interfaceClass = StudentService.class,version = "1.0")
+    private StudentService studentService;
+
+    @GetMapping("/query")
+    public String queryStudent(){
+
+        Student student=studentService.queryStudent(1001);
+        return "调用远程接口，获取对象"+student;
+    }
+
+}
