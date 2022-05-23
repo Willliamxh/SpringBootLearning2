66d8860947b0cf832a874787ee6f75689a2b08de
Williamxh <821602702@qq.com>
2022-05-23 14:38:56 +0800

    7.9 提供者的Component注解可以不用加，注解中的interfaceClass可以不用加

--- 023-service-provider/src/main/java/com/bjp/service/impl/StudentServiceImpl.java
+++ 023-service-provider/src/main/java/com/bjp/service/impl/StudentServiceImpl.java
@@ -14,7 +14,12 @@ import org.springframework.stereotype.Component;
 /**
  * 使用dubbo中的注解暴露服务
  */
-@Component //注入到容器中
+// @Component //注入到容器中
+
+/**
+ * Component可以不用加
+ * interfaceClass也可以不用加
+ */
 @DubboService(interfaceClass = StudentService.class,version = "1.0",timeout = 15000)
 public class StudentServiceImpl implements StudentService {
 
@@ -23,11 +28,11 @@ public class StudentServiceImpl implements StudentService {
         Student student  = new Student();
         if( 1001 == id){
             student.setId(1001);
-            student.setName("------1001-张三");
+            student.setName("====1001-张三");
             student.setAge(20);
         } else if(1002  == id){
             student.setId(1002);
-            student.setName("#######1002-李四");
+            student.setName("#*****1002-李四");
             student.setAge(22);
         }
 
--- 024-consumer/src/main/java/com/bjp/controller/DubboController.java
+++ 024-consumer/src/main/java/com/bjp/controller/DubboController.java
@@ -17,13 +17,16 @@ public class DubboController {
     /**
      * 引用远程服务， 把创建好的代理对象，注入给studentService
      */
-    @DubboReference(interfaceClass = StudentService.class,version = "1.0")
+    // @DubboReference(interfaceClass = StudentService.class,version = "1.0")
+    /**
+     * 没有使用interfaceclass 默认就是引用类型的数据类型
+     */
+    @DubboReference(version = "1.0")
     private StudentService studentService;
 
     @GetMapping("/query")
-    public String queryStudent(){
-
-        Student student=studentService.queryStudent(1001);
+    public String queryStudent(Integer id){
+        Student student=studentService.queryStudent(id);
         return "调用远程接口，获取对象"+student;
     }
 
