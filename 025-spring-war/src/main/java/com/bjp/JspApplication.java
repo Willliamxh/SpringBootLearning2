47070dce13d0793eb36deed6625b514dde578440
Williamxh <821602702@qq.com>
2022-05-28 10:21:15 +0800

    8.1 发布war项目到tomcat：将war项目放到C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps 目录中，然后去：C:\Program Files\Apache Software Foundation\Tomcat 9.0\bin 点击startup.bat; 访问http://localhost:8080/myboot/main；

--- 025-spring-war/src/main/java/com/bjp/JspApplication.java
+++ 025-spring-war/src/main/java/com/bjp/JspApplication.java
@@ -2,12 +2,22 @@ package com.bjp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * SpringBootServletInitializer: 继承这个类， 才能使用独立tomcat服务器
+ */
 @SpringBootApplication
-public class JspApplication {
+public class JspApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JspApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+		return builder.sources(JspApplication.class);
+	}
 }
