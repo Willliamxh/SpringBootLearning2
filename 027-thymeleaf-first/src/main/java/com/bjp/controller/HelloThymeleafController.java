5a6595c444b3202c81fe171f92ab72cae8834f2b
Williamxh <821602702@qq.com>
2022-05-29 10:16:48 +0800

    10.2 模版引擎常用设置

--- 027-thymeleaf-first/src/main/java/com/bjp/controller/HelloThymeleafController.java
+++ 027-thymeleaf-first/src/main/java/com/bjp/controller/HelloThymeleafController.java
@@ -1,6 +1,7 @@
 package com.bjp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,8 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HelloThymeleafController {
     @GetMapping("/hello")
-    public String helloThymeleaf(HttpServletRequest request){
+    public String helloThymeleaf(Model model, HttpServletRequest request){
+        //添加数据到request作用域， 模板引擎可以从request中获取数据
         request.setAttribute("data","欢迎使用thymeleaf");
+
+        //也可以使用model来存放对象
+        model.addAttribute("mydata","model也可以用来存放数据");
+
 
         //指定视图（模板引用使用的页面（html））
         //逻辑名称  classpath:/templates/hello.html
